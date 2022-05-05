5c3eafe005b0e27b53b2e7d183e539b77d352d43
jenna-graham <jclayto2@gmail.com>
2022-05-05 16:00:05 -0700

    started on click handler, if warrior hits dragon

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // import functions and grab DOM elements
 import { renderDragon } from './utils.js';
+const defeatedNumber = document.querySelector('defeated-number');
+const warriorHpEl = document.querySelector('warrior-hp');
 const form = document.querySelector('form');
 const dragonList = document.querySelector('.dragons');
 
@@ -9,6 +11,8 @@ let dragons = [
     { name: 'Drogon', hp: 5 },
     { name: 'Smaug', hp: 2 },
 ];
+let defeatedDragonCount = 0;
+let warriorHp = 0;
 
 // set event listeners 
 form.addEventListener('submit', (e) => {
@@ -35,10 +39,38 @@ function displayDragons() {
 
     for (let dragon of dragons) {
         const dragonEl = renderDragon(dragon);
+        
+
+        dragonEl.addEventListener('click', () =>{
+            dragonClicker(dragon);
+        });
         dragonList.append(dragonEl);
+
     }
 
 }
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+displayDragons();
+
+function dragonClicker(dragon) {
+    console.log(`i am fighting ${dragon.name}!`);
+
+    if (dragon.hp === 0) return;
+    if (player.hp === 0) return;
+
+    const warriorHit = Math.random();
+
+    if (warriorHit < .5) {
+        dragon.hp--;
+        displayDragons();
+        alert(`you did it! you hit ${dragon.name}!`);
+
+        if (dragon.hp === 0) {
+            defeatedDragonCount++;
+            defeatedNumber.textContent = defeatedDragonCount;
+            alert (`You are a fine warrior! ${dragon.name} has died, XXX `)
+        }
+
+
+    }
+}
+
